a13a105626d074efed91843485d18bfa78571c0f
pranmast <42545826+pranmast@users.noreply.github.com>
2026-04-27 01:32:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ document.getElementById("micBtn").onclick = async () => {
     const formData = new FormData();
     formData.append("file", audioBlob, "speech.wav");
 
-    const res = await fetch("https://pranilm-mbti.hf.space", {
+    const res = await fetch("https://pranilm-mbti.hf.space/speech", {
       method: "POST",
       body: formData
     });
